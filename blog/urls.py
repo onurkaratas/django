be1a33c323e5f27ad429af012eee911192c73737
Onur Karatas <onur.karatas@sahibinden.com>
2018-06-21 20:11:56 +0300

    scrappy-usage updated

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     url(r'^blog' , views.post_list, name="post_list"),
     url(r'^cripto', views.cripto_app, name="cripto"),
     url(r'^hello', views.hello, name="hello"),
-    url(r'^git-guide', views.git_guide, name="git-guide")
-    url(r'^scrapy-kullanimi', views.git_guide, name="scrapy-usage")
+    url(r'^git-guide', views.git_guide, name="git-guide"),
+    url(r'^scrapy-guide', views.scrapy_guide, name="scrapy-guide")
 ]
